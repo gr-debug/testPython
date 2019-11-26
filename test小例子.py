3cf5c77cd6020e8add668a31d6ad21b0bceab45b
gr-debug <58216848+gr-debug@users.noreply.github.com>
2019-11-26 21:35:21 +0800

    Add files via upload
    一个关于python while循环的小例子

---
+++ test小例子.py
@@ -0,0 +1,24 @@
+__author__='gr'
+# -- coding:utf-8 --
+#!/usr/bin/env python3
+
+print("Type integer,each followed by Enter;or just Enter to finish")
+total = 0
+count = 0
+
+while True:
+    line = input("integer:")
+    if line:
+        try:
+            number = int(line)
+        except ValueError as err:
+            print(err)
+            continue
+        total +=number
+        count +=1
+    else:
+        break
+
+if count:
+    print("count=",count,"total=",total,"mean=",total/count)
+
